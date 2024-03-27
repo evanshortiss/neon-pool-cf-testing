e2c453760719abd6c39a94e8996137d71dd6ddfe
Andrés Chacón Miranda <28637127+achaconm2001@users.noreply.github.com>
2024-03-27 00:14:45 -0600

    Add WebSocket configuration for local instance

--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -1,5 +1,17 @@
-import { Pool } from '@neondatabase/serverless';
+import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
+import ws from 'ws';
 
+// neonConfig.webSocketConstructor = ws;
+
+if (!process.env.VERCEL_ENV) {
+    // Set the WebSocket proxy to work with the local instance
+    neonConfig.wsProxy = (host) => `${host}:5433/v1`;
+    // Disable all authentication and encryption
+    neonConfig.useSecureWebSocket = false;
+    neonConfig.pipelineTLS = false;
+    neonConfig.pipelineConnect = false;
+  }
+  
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool)
