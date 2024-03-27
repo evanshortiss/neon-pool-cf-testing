8be790210936f197d5dbcb03f3e88c6f9c073f8a
Andrés Chacón Miranda <28637127+achaconm2001@users.noreply.github.com>
2024-03-27 00:09:00 -0600

    Remove unused runtime variable and fix SQL query in test route

--- src/app/test/route.ts
+++ src/app/test/route.ts
@@ -2,7 +2,6 @@ import { db } from "@/lib/drizzle";
 import { sql } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
-export const runtime=   "edge";
 
 export async function GET() {
     const posts = await db.execute(sql`SELECT * FROM NOW()`	);
