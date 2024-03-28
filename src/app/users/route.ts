4efd59be3ba546bc9e9c0c2de5673b2dcf295d6f
Andrés Chacón Miranda <28637127+achaconm2001@users.noreply.github.com>
2024-03-28 01:53:42 -0600

    Update function to handle edge cases

---
+++ src/app/users/route.ts
@@ -0,0 +1,12 @@
+import { getDb } from "@/lib/drizzle";
+import { sql } from "drizzle-orm";
+import { unstable_noStore } from "next/cache";
+import { NextResponse } from "next/server";
+
+export const runtime=   "edge";
+
+export async function GET() {
+  unstable_noStore()
+  const posts = await getDb().execute(sql`SELECT * FROM users;`);
+  return NextResponse.json(posts);
+}
